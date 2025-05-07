4290fdcb2362436d0459b4ccda44bcc61cf3d7d5
XFM Operator <xf04bm@xf04bm-ws1.nsls2.bnl.local>
2025-05-07 15:38:56 -0400

    use RedisJsonDict to store metadata
     * also restore olog initialization

--- startup/00-base.py
+++ startup/00-base.py
@@ -6,6 +6,16 @@ nslsii.configure_base(get_ipython().user_ns,
 
 #nslsii.configure_olog(get_ipython().user_ns)
 
-#Optional: set any metadata that rarely changes.
-RE.md['beamline_id'] = 'XFM'
+import redis
+from redis_json_dict import RedisJSONDict
 
+uri = "info.xfm.nsls2.bnl.gov"
+# Provide an endstation prefix, if needed, with a trailing "-"
+new_md = RedisJSONDict(redis.Redis(uri),prefix="")
+#BEAMLINE_ID = 'xfm'
+
+nslsii.configure_olog(get_ipython().user_ns)
+
+#Optional: set any metadata that rarely changes.
+#RE.md['beamline_id'] = 'XFM'
+RE.md = new_md
